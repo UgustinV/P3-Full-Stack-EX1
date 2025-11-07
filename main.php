81e1b96efc7d0097101f75eea8623de3a924c486
Augustin Viard <gugusviard@gmail.com>
2025-11-07 11:05:16 +0100

    feat/implemented detail command detection and getting id

--- main.php
+++ main.php
@@ -7,4 +7,9 @@ while (true) {
     if ($line === "list") {
         $command->list();
     }
+    $matches = [];
+    if(preg_match('/^detail ([0-9]+)$/', $line, $matches)) {
+        $id = $matches[1];
+        var_dump($id);
+    }
 }
