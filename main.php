5e40a0f05ee401adc9bbe60491dbcfd8fb8f60bc
Augustin Viard <gugusviard@gmail.com>
2025-11-07 10:39:39 +0100

    feat/list command done

--- main.php
+++ main.php
@@ -3,12 +3,13 @@ require_once 'db_connect.php';
 require_once 'contact_manager.php';
 
 $db = DBConnect::getPDO('localhost', 'p3-ex1', 'root', '');
-$contactManager = new ContactManager($db);
-$contact_list = $contactManager->findAll();
-var_dump($contact_list);
 while (true) {
     $line = readline("Entrez votre commande : ");
     if ($line === "list") {
-        echo "Affichage de la liste";
+        $contactManager = new ContactManager($db);
+        $contact_list = $contactManager->findAll();
+        foreach ($contact_list as $contact) {
+            echo $contact->__toString() . "\n";
+        }
     }
 }
